559e4cbb9dc68280cd1c0e8961115ef252a230c2
Abu Hasan <ranaabuhasan@gmail.com>
2021-08-17 16:16:03 +0600

    first-commit

---
+++ calculator-1.js
@@ -0,0 +1,13 @@
+
+/* const buttons = document.getElementsByClassName('btn');
+for(const button of buttons){
+    button.addEventListener('click', function(){
+        const buttonValue = button.value;
+
+        /* output result */
+       /*  const outputChecked = document.getElementById('output-result');
+        const valueOutput = outputChecked.value;
+        outputChecked.value = buttonValue;
+        outputChecked.value = outputChecked.value + buttonValue;
+    }); */
+//} */
